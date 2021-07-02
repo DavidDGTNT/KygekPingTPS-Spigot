615ae5a7c5066d6f5379b4eca52bbe7b548f3f71
Kygekraqmak <techfixindonesia@gmail.com>
2021-07-02 08:05:34 +0800

    Added license notice to source code

--- src/org/kygekteam/java/kygekpingtps/Ping.java
+++ src/org/kygekteam/java/kygekpingtps/Ping.java
@@ -1,3 +1,13 @@
+/*
+ * See server TPS and a player's ping
+ * Copyright (C) 2021 KygekTeam
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 package org.kygekteam.java.kygekpingtps;
 
 import org.bukkit.ChatColor;
